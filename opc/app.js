2cd86da26fc054da66649d4a8cc572b7395b952d
gabriela <bedoyagabriela28@gmail.com>
2024-11-24 16:36:46 -0500

    proyectoInicial

---
+++ opc/app.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper('.mySwiper-1', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 5000,
+        },
+    });
+});
